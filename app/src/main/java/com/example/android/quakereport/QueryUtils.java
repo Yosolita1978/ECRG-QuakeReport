129070152eae92b80341be285e8be5c153fb79ad
cristina <yosola@gmail.com>
2018-07-19 13:30:31 -0700

    UI best practices for loading data

--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -186,6 +186,11 @@ public final class QueryUtils {
 
         // Perform HTTP request to the URL and receive a JSON response back
         String jsonResponse = null;
+//        try {
+//            Thread.sleep(2000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         try {
             jsonResponse = makeHttpRequest(url);
         } catch (IOException e) {
